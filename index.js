c9bc1549d6837646d59ab43837a051def78d6df2
Yoshi01O <22211370yy@tama.ac.jp>
2024-02-21 03:54:13 +0900

    Update

---
+++ index.js
@@ -0,0 +1,13 @@
+function OnColor(ele) {
+    var element = ele;
+    element.style.backgroundColor="white";
+    element.style.color="black";
+    console.log(element);
+}
+
+function OffColor(ele) {
+    var element = ele;
+    element.style.backgroundColor="gray";
+    element.style.color="white";
+    console.log(element);
+}
