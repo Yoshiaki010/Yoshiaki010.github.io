61a50be7a5ab1b46db9019ce64d8860717812962
Yoshi01O <22211370yy@tama.ac.jp>
2024-02-23 23:31:01 +0900

    Update

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ function OnColor(ele) {
     var element = ele;
     element.style.backgroundColor="white";
     element.style.color="black";
-    console.log(element);
 }
 
 function OffColor(ele) {
     var element = ele;
     element.style.backgroundColor="gray";
     element.style.color="white";
-    console.log(element);
 }
