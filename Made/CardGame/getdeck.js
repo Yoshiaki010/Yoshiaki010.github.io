ac511a1fb811e6475175a7c3ea47c37b6673271d
Yoshi01O <22211370yy@tama.ac.jp>
2024-09-08 23:53:28 +0900

    デッキの取得方法わからず...

--- Made/CardGame/getdeck.js
+++ Made/CardGame/getdeck.js
@@ -1,19 +1,25 @@
 // URLにアクセスしてデータを取得
 const url = 'https://www.pokemon-card.com/deck/deck.html?deckID=NHHggi-4r7wBe-gHngNH';
 
+let text = document.getElementById('test').textContent; //宣言
 fetch(url)
   .then(response => {
     // レスポンスが正常かどうかをチェック
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    return response.text(); // レスポンスをテキストとして返す
+    return response.json(); // レスポンスをテキストとして返す
   })
   .then(data => {
     // 取得したデータを処理
-    console.log("ok"); // データをコンソールに表示
+    console.log(response.json); // データをコンソールに表示
+    document.getElementById('test').textContent = 'ok';
+    
   })
   .catch(error => {
     // エラーが発生した場合の処理
     console.error('There has been a problem with your fetch operation:', error);
+    document.getElementById('test').textContent = 'error';
   });
+
+
